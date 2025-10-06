1419d162639bb73609139e2f89fcae503ae8f74b
PhucPhuc1111 <phuctoi8930@gmail.com>
2025-10-06 12:04:53 +0700

    config Ocelot

---
+++ FundService/Fund.Common/BusinessResult/IBusinessResult.cs
@@ -0,0 +1,9 @@
+﻿namespace NutriDiet.Common.BusinessResult
+{
+    public interface IBusinessResult
+    {
+        int StatusCode { get; set; }
+        string? Message { get; set; }
+        object? Data { get; set; }
+    }
+}
